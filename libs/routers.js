35c640ed46605c4d1e1a5552556a6b39a18daa13
root <root@localhost.localdomain>
2017-09-08 17:09:29 -0400

    First Commit

---
+++ libs/routers.js
@@ -0,0 +1,119 @@
+FlowRouter.route('/',{
+	name: "Home",
+	action(){
+		BlazeLayout.render('HomeLayout');
+	}
+})
+
+FlowRouter.route('/streaming',{
+	name: 'streaming',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Streaming' });
+	}
+})
+
+FlowRouter.route('/programs',{
+	name: 'programs',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Programs' });
+	}
+})
+
+FlowRouter.route('/schedule',{
+	name: 'schedule',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Schedule' });
+	}
+})
+
+FlowRouter.route('/producers',{
+	name: 'producers',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Producers' });
+	}
+})
+
+FlowRouter.route('/member',{
+	name: 'member',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Member' });
+	}
+})
+
+FlowRouter.route('/gifts',{
+	name: 'gifts',
+	action(){
+		BlazeLayout.render('HomeLayout', { main: 'Gifts' });
+	}
+})
+
+FlowRouter.route('/wish-list',{
+	name: 'wish-list',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Wishlist'});
+	}
+})
+
+FlowRouter.route('/donatemusic',{
+	name:'donatemusic',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Donatemusic' });
+	}
+})
+
+FlowRouter.route('/volunteer',{
+	name: 'volunteer',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Volunteer' });
+	}
+})
+
+FlowRouter.route('/produce-a-show',{
+	name: 'produce-a-show',
+	action(){
+		BlazeLayout.render('HomeLayout', { main: 'ProduceAshow' });
+	}
+})
+
+FlowRouter.route('/underwrite',{
+	name: 'underwrite',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Underwrite' }); 
+	}
+})
+
+FlowRouter.route('/mission',{
+	name: 'mission',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Mission'});
+	}
+})
+
+FlowRouter.route('/lpfm',{
+	name: 'lpfm',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Lpfm' }); 
+	}
+})
+
+FlowRouter.route('/supporters',{
+	name:'supporters',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Supporters' });
+	}
+})
+
+FlowRouter.route('/boardmembers',{
+	name: 'boardmembers',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Boardmembers' });
+	}
+})
+
+FlowRouter.route('/contact',{
+	name: 'contact',
+	action(){
+		BlazeLayout.render('HomeLayout',{ main: 'Contact' });
+	}
+})
+
