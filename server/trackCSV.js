f57c52d4af5b91347a0dee91d6dcdbf432a2ca33
neobii <iwilldevelopcodeforyou@gmail.com>
2018-01-02 14:12:55 -0600

    proper csv formatting yay!

--- server/trackCSV.js
+++ server/trackCSV.js
@@ -1,19 +1,19 @@
 Meteor.methods({
   downloadTracksCSV(dateFrom, dateTo) {
+    var tracksArray = [];
     tracks = Tracklists.find({ 'playDate' : { $gte : dateFrom, $lt: dateTo }}, {fields: {playDateOffset: 0, userId: 0, showId: 0, isQueuedForNext: 0}, sort: {playDate: 1}}).fetch();
-      tracks.forEach(function(v){ 
-        delete v._id 
-      });
-      tracks = _.map(tracks, function(track){
-        track.albumName = track.albumName || "NA";
-        track.songTitle = track.songTitle || "NA";
-        track.artist = track.artist || "NA";
-        track.label = track.label || "NA";
-        track.trackLength = track.trackLength || "NA";
-        return track;
+      tracks = _.each(tracks, function(track){
+        var cleanTrack = {};
+        cleanTrack.albumName = track.albumName || "NA";
+        cleanTrack.songTitle = track.songTitle || "NA";
+        cleanTrack.artist = track.artist || "NA";
+        cleanTrack.label = track.label || "NA";
+        cleanTrack.trackLength = track.trackLength || "NA";
+        tracksArray.push(cleanTrack);
       })
       var heading = true;
       var delimiter = "|";
-   return exportcsv.exportToCSV(tracks, heading, delimiter);
+      //console.log(tracksArray);
+   return exportcsv.exportToCSV(tracksArray, heading, delimiter);
   },
 });
