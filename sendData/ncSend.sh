b0caff16ead22ecb4eb9cf299f001550c1830d33
neobii <iwilldevelopcodeforyou@gmail.com>
2018-01-02 07:51:43 -0600

    bumped versions and added new json formatting code

--- sendData/ncSend.sh
+++ sendData/ncSend.sh
@@ -3,12 +3,32 @@
 while true;do
   data=$(nc -l -p 9876);
   IFS='|'
-  while -r artist track album tracklength comments 
-  do
-    "${ADDR[$COUNTER]}";  
-  done < "$data"
+  json=""
+  counter=0
+  for item in $(echo "$data"); do
+    if [ $counter -eq 0 ]
+      then
+        json="{'artist': '$item', "
+    fi
+    if [ $counter -eq 1 ]
+      then
+        json="$json'songTitle': '$item', "
+    fi
+    if [ $counter -eq 3 ]
+      then
+        json="$json'album': '$item', "
+    fi
+    if [ $counter -eq 4 ]
+      then
+        json="$json'label': '$item', "
+    fi
+    if [ $counter -eq 5 ]
+      then
+        json="$json'trackLength': '$item'}"
+    fi
+    counter=$((counter+1))
+  done
+  unset IFS
   #curl -X POST -H "Content-Type: application/json" -d "$data" "http://producer.kuzu.fm/methods/insertTrack" &
   #curl -X POST -H "Content-Type: application/json" -d "$data" "http://localhost:3000/methods/insertTrack" &
 done
-
-"blah|blah|blah|blah|blah"
