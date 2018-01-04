8d7da0555045cf2910d405c80a28bab203fa642a
Adam <iwilldevelopcodeforyou@gmail.com>
2018-01-03 19:25:42 -0600

    Update ncSend.sh

--- sendData/ncSend.sh
+++ sendData/ncSend.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
 while true;do
-  data=$(nc -l -v 9876); # -p for linux bash
+  data=$(nc -l -v 9876);
+  echo $data;
   IFS='|'
   json=""
   counter=0
   for item in $(echo "$data"); do
     item=`echo $item | sed -e 's|\"|\\\"|g'`
+    item=`echo $item | sed -e 's|^ ||g'`
     item=`echo $item | sed -e 's| $||g'`
-    if [ $counter -eq 0 ]
+    if [ $counter -eq 1 ]
       then
         json="{\"artist\": \"$item\", "
     fi
-    if [ $counter -eq 1 ]
+    if [ $counter -eq 2 ]
       then
         json="$json\"songTitle\": \"$item\", "
     fi
-    if [ $counter -eq 2 ]
+    if [ $counter -eq 3 ]
       then
         json="$json\"album\": \"$item\", "
     fi
-    if [ $counter -eq 3 ]
+    if [ $counter -eq 4 ]
       then
         json="$json\"label\": \"$item\", "
     fi
-    if [ $counter -eq 4 ]
+    if [ $counter -eq 5 ]
       then
         json="$json\"trackLength\": \"$item\"}"
     fi
     counter=$((counter+1))
   done
   unset IFS
-  #echo $json
-  #curl -X POST -H "Content-Type: application/json" -d "$json" "http://producer.kuzu.fm/methods/insertTrack" &
+  echo $json
+  curl -X POST -H "Content-Type: application/json" -d "$json" "http://producer.kuzu.fm/methods/insertTrack" &
   #curl -X POST -H "Content-Type: application/json" -d "$json" "http://localhost:3000/methods/insertTrack" &
 done
