47cb41c46c85c028b2f38cc3b4bfb11a12b1f7f9
Adam <iwilldevelopcodeforyou@gmail.com>
2018-01-03 20:10:47 -0600

    Update nicecast.sh

--- sendData/nicecast.sh
+++ sendData/nicecast.sh
@@ -1,1 +1,6 @@
-curl http://localhost:3000/methods/getNicecastMeta | sed "s/|/\\r\\n/g" | sed "s/\"//g" > myFile.txt
+while true; do
+cleanString=$(echo `curl -s http://producer.kuzu.fm/methods/getNicecastMeta | sed "s/^\"//g" | sed "s/\"$//g"` | sed "s/|/\\\r\\\n/g")
+echo -e $cleanString > "/Users/kuzu/Library/Application Support/Nicecast/NowPlaying.txt"
+sleep 1
+done
+
