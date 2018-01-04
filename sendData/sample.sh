41cec2104681df63f72f0cf4a81759d5782bba97
neobii <iwilldevelopcodeforyou@gmail.com>
2018-01-04 01:52:20 -0600

    added fix for search when there is no user

--- sendData/sample.sh
+++ sendData/sample.sh
@@ -1,2 +1,2 @@
 #cat track.json | nc -q 0 127.0.0.1 9876
-echo 'blah|blah|blah|blah|"blah' | nc -q 0 127.0.0.1 9876
+echo '| Carol | So Low | Breakdown | Dirty Dance 7003 | 03:51' | nc -q 0 127.0.0.1 9876
