7fcf85d029b33a2e993cbbaed7529f85fef70e75
neobii <iwilldevelopcodeforyou@gmail.com>
2018-01-25 12:22:24 -0600

    clean up

--- client/helpers.js
+++ client/helpers.js
@@ -1,4 +1,7 @@
 Template.registerHelper("prettifyTime", (date) => {
+  if(!date) {
+    return "";
+  }
   var date = new moment(date).format("hh:mm:ss");
   return date;
 })
--- server/producerParse.js
+++ server/producerParse.js
@@ -4,14 +4,15 @@ Meteor.methods({
     for ( let i = 0; i < data.length; i++ ) {
       let item   = data[ i ];
       var regex = /%(.*)%/;
-      if(item && item.Name){
+      if(item && item.Name) {
+        var trackItem;
         var match = item.Meta && item.Meta.match(regex)[1];
         if(!match) {
           var names = item.Name.split("|");
           if(!names[1]){
             continue;
           }
-          var trackItem = {
+          trackItem = {
             artist: names[0],
             songTitle: names[1],
             album: item.Album,
@@ -19,16 +20,18 @@ Meteor.methods({
             trackLength: item.Length,
             showId: currentShowId
           };
-          Tracklists.insert(trackItem);
         } else {
-          var trackItem = {
+          trackItem = {
             artist: " ",
             songTitle: item.Name,
+            album: " ",
+            label: " ",
             trackLength: item.Length,
             showId: currentShowId,
             trackType: match
           };
         }
+        Tracklists.insert(trackItem);
       }
     }
   }
