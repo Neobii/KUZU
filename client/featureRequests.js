9a41a30ba16e6c8591544c116eab1c040fe95797
neobii <iwilldevelopcodeforyou@gmail.com>
2018-02-02 05:26:55 -0600

    democrazy established

--- client/featureRequests.js
+++ client/featureRequests.js
@@ -9,3 +9,14 @@ Template.featureRequests.helpers({
     return FeatureRequests.find({});
   }
 })
+
+Template.featureRequests.events({
+  "click [data-vote-up]"(e, t) {
+    var currentId = $(e.currentTarget).attr("data-vote-up");
+    FeatureRequests.update({_id: currentId}, {$push: {userVotesUp: Meteor.userId()}, $pull: {userVotesDown: Meteor.userId()}})
+  },
+  "click [data-vote-down]"(e, t) {
+    var currentId = $(e.currentTarget).attr("data-vote-down");
+    FeatureRequests.update({_id: currentId}, {$push: {userVotesDown: Meteor.userId()}, $pull: {userVotesUp: Meteor.userId()}})
+  },
+})
