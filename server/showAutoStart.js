69d691f0d47e052938db0670a330f5c347dc80ad
neobii <iwilldevelopcodeforyou@gmail.com>
2018-02-06 06:42:15 -0600

    autostart and auto end

--- server/showAutoStart.js
+++ server/showAutoStart.js
@@ -1,45 +1,39 @@
-/*
-SyncedCron.add({
-  name: 'Crunch some important numbers for the marketing department',
-  schedule: function(parser) {
-    // parser is a later.parse object
-    return parser.text('every 2 hours');
-  },
-  job: function() {
-    var numbersCrunched = CrushSomeNumbers();
-    return numbersCrunched;
-  }
-});
-*/
-
 Meteor.startup(function(){
   SyncedCron.stop();
-  ////parser.recur().on(date).fullDate();
-  //add all autostart to meteor
+  var shows = Shows.find({autoStartEnd: true}).fetch();
+  _.each(shows, function(show){
+    App.addAutoStartShow(show._id);
+  })
 })
 
 App.addAutoStartShow = function(showId){
   var show = Shows.findOne({_id: showId});
-  SyncedCron.add({
-    name: 'AutoStart_' + show._id,
-    schedule: function(parser) {
-      return parser.recur().on(show.showStart).fullDate();
-    },
-    job: function() {
-      Shows.update({_id: show._id}, {$set: {isActive: false}}, {multi: true})
-      Shows.update({_id: show._id}, {$set: {isActive: true}});
-      Meteor.call("autoplayNextTrack");
-    }
-  });
-  SyncedCron.add({
-    name: 'AutoEnd_' + show._id,
-    schedule: function(parser) {
-      return parser.recur().on(show.showEnd).fullDate();
-    },
-    job: function() {
-      Shows.update({_id: show._id}, {$set: {isActive: false, autoStartEnd: false}})
-    }
-  });
+  if(new Date(show.showStart).getTime() > new Date().getTime()){
+    SyncedCron.add({
+      name: 'AutoStart_' + show._id,
+      schedule: function(parser) {
+        return parser.recur().on(show.showStart).fullDate();
+      },
+      job: function() {
+        Shows.update({_id: show._id}, {$set: {isActive: false}}, {multi: true})
+        Shows.update({_id: show._id}, {$set: {isActive: true}});
+        Meteor.call("autoplayNextTrack");
+        SyncedCron.remove("AutoStart_" + show._id);
+      }
+    });
+  }
+  if(new Date(show.showEnd).getTime() > new Date().getTime()){
+    SyncedCron.add({
+      name: 'AutoEnd_' + show._id,
+      schedule: function(parser) {
+        return parser.recur().on(show.showEnd).fullDate();
+      },
+      job: function() {
+        Shows.update({_id: show._id}, {$set: {isActive: false, autoStartEnd: false}});
+        SyncedCron.remove("AutoEnd_" + show._id);
+      }
+    });
+  }
   SyncedCron.start();
 }
 
