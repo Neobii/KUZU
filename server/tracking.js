50ffb5d5f8429cabf5a4f30e6d9fc022dd8af0e8
Dan Minshew <ofenixculpa@gmail.com>
2019-01-31 23:33:14 -0600

    remove access control header

--- server/tracking.js
+++ server/tracking.js
@@ -141,7 +141,6 @@ Meteor.method("getLastTracks", function(numTracks) {
 JsonRoutes.setResponseHeaders({
   "Cache-Control": "no-store",
   "Pragma": "no-cache",
-  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
 });
